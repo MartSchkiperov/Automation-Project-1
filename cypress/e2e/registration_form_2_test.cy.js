823d6141820db19ad8ee440ba8279eb9903b3522
MartSchkiperov <martpikk@gmail.com>
2024-05-09 19:41:46 +0300

    form_2 assignment 4

--- cypress/e2e/registration_form_2_test.cy.js
+++ cypress/e2e/registration_form_2_test.cy.js
@@ -10,33 +10,91 @@ describe('Section 1: Functional tests', () => {
 
     it('User can use only same both first and validation passwords', ()=>{
         // Add test steps for filling in only mandatory fields
+        cy.get('#username').type('Something')
+        cy.get('#email').type('e@e.ee')
+        cy.get('[data-cy="name"]').type('Name')
+        cy.get('[data-testid="lastNameTestId"]').type('Lastname')
+        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+        cy.get('#password').type('MyPass123')
         // Type confirmation password which is different from first password
+        cy.get('#confirm').type('MyPass12')
+        cy.get('h2').contains('Password').click()
+                
         // Assert that submit button is not enabled
+        cy.get('.submit_button').should('be.disabled')
         // Assert that successful message is not visible
+        cy.get('#success_message').should('not.be.visible')
         // Assert that error message is visible
+        cy.get('#password_error_message').should('be.visible')
+
         // Change the test, so the passwords would match
+        cy.get('#confirm').clear().type('MyPass123')
+        cy.get('h2').contains('Password').click()
         // Add assertion, that error message is not visible anymore
+        cy.get('#password_error_message').should('not.be.visible')
         // Add assertion, that submit button is now enabled
+        cy.get('.submit_button').should('be.enabled')
     })
     
     it('User can submit form with all fields added', ()=>{
         // Add test steps for filling in ALL fields
+        cy.get('#username').type('Something')
+        cy.get('#email').type('e@e.ee')
+        cy.get('[data-cy="name"]').type('Name')
+        cy.get('[data-testid="lastNameTestId"]').type('Lastname')
+        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+        cy.get('#htmlFavLanguage').click()
+        cy.get('#vehicle1').click()
+        cy.get('#vehicle2').click()
+        cy.get('#vehicle3').click()
+        cy.get('#password').type('MyPass123')
+        cy.get('#confirm').type('MyPass123')
+        cy.get('h2').contains('Password').click()
         // Assert that submit button is enabled
+        cy.get('.submit_button').should('be.enabled')
         // Assert that after submitting the form system show successful message
+        cy.get('.submit_button').click()
+        cy.get('#success_message').should('be.visible')
     })
 
     it('User can submit form with valid data and only mandatory fields added', ()=>{
         // Add test steps for filling in ONLY mandatory fields
-        // Assert that submit button is enabled
-        // Assert that after submitting the form system shows successful message
-
         // example, how to use function, which fills in all mandatory data
         // in order to see the content of the function, scroll to the end of the file
         inputValidData('johnDoe')
+
+        // Assert that submit button is enabled
+        cy.get('.submit_button').should('be.enabled')
+
+        // Assert that after submitting the form system shows successful message
+        cy.get('.submit_button').click()
+        cy.get('#success_message').should('be.visible')
     })
 
     // Add at least 1 test for checking some mandatory field's absence
+    it('User must fill mandatory email field', ()=>{
+        // Add test steps for filling in only mandatory fields
+        cy.get('#username').type('Something')
 
+        cy.get('[data-cy="name"]').type('Name')
+        cy.get('[data-testid="lastNameTestId"]').type('Lastname')
+        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+        cy.get('#password').type('MyPass123')
+        cy.get('#confirm').type('MyPass123')
+        cy.get('h2').contains('Password').click()
+                
+        // Assert that submit button is not enabled
+        cy.get('.submit_button').should('be.disabled')
+
+        // Add e-mail address
+        cy.get('#email').type('e@e.ee')
+        cy.get('h2').contains('Password').click()
+        // Filling e-mail the submit button is now enabled, it is possible to click it
+        // and success message is given
+        cy.get('.submit_button').should('be.enabled')
+        cy.get('.submit_button').click()
+        cy.get('#success_message').should('be.visible')
+    })
 })
 
 /*
